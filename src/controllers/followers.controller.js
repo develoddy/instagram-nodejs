bb12f36fcb38dd0f34a5f536013473201b83d0c0
Lujandev <eddylujann@gmailc.om>
2023-05-15 16:04:15 +0200

    follower

--- src/controllers/followers.controller.js
+++ src/controllers/followers.controller.js
@@ -1,9 +1,28 @@
 import { Followers } from '../models/Followers.js';
+import { Userfollowers } from '../models/Userfollowers.js';
 
 export const getFollowers = async ( req, res ) => {
 };
 
 export const createFollowers = async ( req, res ) => {
+    const { userId, followerId } = req.body;
+
+    try {
+        const newFollower = await Followers.create({
+            userId
+        });
+
+        console.log(typeof newFollower);
+        if ( newFollower ) {
+            const newUserfollower = await Userfollowers.create({
+                followerId
+            });
+        }
+
+        res.json(newUserfollower);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
 };
 
 export const updateFollowers = async ( req, res ) => {
